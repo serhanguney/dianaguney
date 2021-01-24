94c97f503a453c8f7130255a5016b31da8a5281c
yuivae <gnyserhan@gmail.com>
2021-01-25 00:44:03 +0300

    meta put index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./App";
-import { render } from "react-snapshot";
+// import { render } from "react-snapshot";
 
-render(
+ReactDOM.render(
   <App />,
 
   document.getElementById("root")
