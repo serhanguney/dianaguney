ebff47f6064819590120199eb559b541961c144d
Serhan Guney <67260021+serhanguney@users.noreply.github.com>
2022-03-19 16:41:39 +0100

    add NYOI and Loki (#7)
    * add NYOI and Loki
    
    * update image styling
    
    * update nyoi

--- src/Projects/Films.js
+++ src/Projects/Films.js
@@ -3,28 +3,39 @@ export const baseUrl = "https://yuivae.mo.cloudinary.net";
 export const films = [
   {
     id: 0,
+    projectTitle: "Marvel’s Loki x Hyundai – Question Everything",
+    title: "Marvel’s Loki x Hyundai – Question Everything",
+    text: "Assistant Art Director - Production Company: LS Productions // Bullitt Branded",
+    paragraph: `A commercial for Hyundai that picks up with the God of Mischief moments after he escaped in Avengers: Endgame and before the Time Variance Authority caught up with him in Loki..." - Comicbookmovie.com. 
+    A great opportunity to be a part of a Marvel and Hyundai branded commercial shoot at the centre of London.
+Here are some screenshots and photos behind the scenes.`,
+    photos: [`${baseUrl}/loki_1.jpg`,`${baseUrl}/loki_2.jpg`,`${baseUrl}/loki_3.jpg`,`${baseUrl}/loki_4.jpg`,`${baseUrl}/loki_5.jpg`,`${baseUrl}/loki_6.jpg`],
+    active: false,
+  },
+  {
+    id: 1,
     projectTitle: "Prizefighter",
     title: "Prizefighter: Life of Jem Belcher",
-    text: "Art Director - Production Company: Camelot",
+    text: "Art Director - Production Company: Camelot Films (currently on post-production)",
     paragraph: `Prizefighter was the first project that heavily involved being part of the design and look of the film. I worked with design concepts and preparing drawings for different sets across our locations in the UK and Lithuania.
     Plot: At the turn of the 19th century, Pugilism was the sport of kings and a gifted young boxer fought his way to becoming champion of England.`,
     photos: [`${baseUrl}/film_1.jpg`],
     active: false,
   },
   {
-    id: 1,
+    id: 2,
     projectTitle: "The Power",
     title: "The Power",
-    text: "Assistant Art Director - Production Company: Amazon Prime",
+    text: "Assistant Art Director - Production Company: Sister Pictures (currently on post-production)",
     paragraph: `Based on Naomi Alderman's novel The Power. A group of teenage girls mysteriously develop a special power that allows them to electrocute people at will.`,
     photos: [`${baseUrl}/film_2.jpg`],
     active: false,
   },
   {
-    id: 2,
+    id: 3,
     projectTitle: "Pennyworth",
     title: "Pennyworth Season 3",
-    text: "Assistant Art Director - Production Company: HBO MAX",
+    text: "Assistant Art Director - Production Company: Shoe Bucket (currently on post-production)",
     paragraph: `The origin story of Alfred Pennyworth, a former special-forces soldier living in London and how he came to work for Bruce Wayne's father.  The season begins after a five year time jump and with the civil war at an end, a new cultural revolution has begun to sweep through the world.`,
     photos: [`${baseUrl}/film_3.jpg`],
     active: false,
